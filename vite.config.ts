8f31b36f62efac950c42f128ec52969ee0930766
Steve Saunders <stevenjs@ymail.com>
2023-11-19 21:07:06 +1100

    Cleanup/update dependencies, build and test config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig(() => {
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts',
       css: true,
       reporters: ['verbose'],
       coverage: {
